5f5e67f579730001f15dff11da51ad23cc31caf7
Holovashchenko Mykola <112394547+Kolya14051996@users.noreply.github.com>
2024-01-29 19:18:21 +0200

    ready

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,23 +1,15 @@
 import * as path from 'path';
 import {dirname} from 'node:path';
 import {fileURLToPath} from 'url';
-import fs from 'fs'
+import fs from 'fs';
 
 const write = async () => {
-    const __dirname = dirname(fileURLToPath(import.meta.url));
+	const __dirname = dirname(fileURLToPath(import.meta.url));
 	const sourceFileName = path.join(__dirname, 'files', 'fileToWrite.txt');
 
-    const streamWrite = fs.createWriteStream(sourceFileName)
+	const streamWrite = fs.createWriteStream(sourceFileName);
 
-    process.stdin.pipe(streamWrite)
-
-    // streamWrite.on('finish', () => {
-    //     console.log(`Data written to ${streamWrite}`);
-    //   });
-
-    //   streamWrite.on('error', (error) => {
-    //     console.error(`Error writing to file: ${error.message}`);
-    //   });
+	process.stdin.pipe(streamWrite);
 };
 
 await write();
